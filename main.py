6de1d028336b0907a6caa638005a61dd1c8a1e0e
Zekorei <46651082+Zekorei@users.noreply.github.com>
2025-04-11 01:59:55 -0400

    -

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from items import Item
 OPTIONS = ["temp", "settings", "help", "quit"]
 
 
+# --- main
 def main():
     running = True
     settings = Settings(SETTINGS_CONFIG)
@@ -34,6 +35,6 @@ def main():
             running = False
 
 
-# main
+# ---
 if __name__ == "__main__":
     main()
