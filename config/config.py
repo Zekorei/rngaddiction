6e2c7747baed78bf8583c152c7a7aaed0b16e9d4
Zekorei <46651082+Zekorei@users.noreply.github.com>
2025-04-20 01:20:02 -0400

    added __getitem__ method

--- config/config.py
+++ config/config.py
@@ -32,6 +32,9 @@ class Config:
         cls = self.__class__.__name__
         return f"{cls}(path={self.path!r}, data={self.data!r})"
 
+    def __getitem__(self, key: str) -> str | Any:
+        return self.data[key]
+
     # ---
     def save(self):
         with open(self.path, "w") as stream:
