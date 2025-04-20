72186b412a3bcd02202ab79e35e9a41228924d77
Zekorei <46651082+Zekorei@users.noreply.github.com>
2025-04-20 01:47:43 -0400

    added placement functionality

--- menus.py
+++ menus.py
@@ -2,27 +2,31 @@
 Contains all functions for menus
 """
 
-from interface import Menu
 from config.config import Config, SETTINGS_CONFIG
 from config.settings import Settings
+from interface import Inventory, Menu
+from items import Item
 
 main_config = Config("data/menus/main_menu.json")
 
 class MainMenu:
     def __init__(self):
         self.display = Menu[str](main_config["name"], main_config["options"])
-        self.settings = Settings(SETTINGS_CONFIG)
+        self.inventory = Inventory[Item]("Inventory")
+        self._settings = Settings(SETTINGS_CONFIG)
         self._running = True
 
     # ---
     def play(self) -> ...:
         pass
 
-    def settings(self) -> ...:
-        pass
+    def settings_display(self) -> ...:
+        print("Settings:\n"
+              f"- Cursor [{self._settings.data['cursor']['icon']}]\n"
+              f"- Color: {self._settings.data['cursor']['color']}")
 
-    def help(self) -> ...:
-        pass
+    def help(self) -> None:
+        print("Help Stuff: \n To be added...")
 
     def quit(self) -> None:
         self._running = False
@@ -31,8 +35,12 @@ class MainMenu:
     def running(self) -> bool:
         return self._running
 
+    @property
+    def settings(self) -> Settings:
+        return self._settings
+
     @running.setter
     def running(self, value) -> None:
-        self.settings.save()
+        self._settings.save()
         self._running = value
 
