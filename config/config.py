ad9c350b3336f24a01bcb9e6430f33847b6d3724
Zekorei <46651082+Zekorei@users.noreply.github.com>
2025-04-30 15:15:03 -0400

    changes to file processing

--- config/config.py
+++ config/config.py
@@ -1,3 +1,4 @@
+import os
 from json import load, dumps
 from os import PathLike
 from typing import Any
@@ -11,34 +12,66 @@ SETTINGS_CONFIG = "settings.json"
 ITEM_CONFIG = "item_data.json"
 
 
+class File:
+    def __init__(self, file_path: str | PathLike[str]) -> None:
+        self._path = file_path
+        self._name = self._path.split(os.pathsep)[-1]
+        self._type = self._name.split(".")[-1]
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self._path})"
+
+    # ---
+    @property
+    def path(self) -> str | PathLike[str]:
+        return self._path
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def type(self) -> str:
+        return self._type
+
+
+# TODO: generalize to a json object with path lookup
 class Config:
     @staticmethod
-    def load(f_path: str | PathLike[str]) -> JSONDict:
-        with open(f_path, "r") as stream:
+    def load(file_path: str | PathLike[str]) -> JSONDict:
+        with open(file_path, "r") as stream:
             data: JSONDict = load(stream)
 
         return data
 
-    # ---
-    def __init__(self, path: str | PathLike[str]) -> None:
-        self.path = path
+    @staticmethod
+    def file_name(file_path: str | PathLike[str]) -> str:
+        return
 
-        if path.endswith(".json"):
-            self.data = self.load(path)
+    @staticmethod
+    def file_type(file: str):
+        return
+
+    # ---
+    def __init__(self, file_path: str | PathLike[str]) -> None:
+        self.file = File(file_path)
+
+        if self.file.type == "json":
+            self.data = self.load(self.file.path)
         else:
-            raise TypeError("Unable to read file")
+            raise TypeError(f"Unable to read file (.{self.file.type})")
 
     def __repr__(self) -> str:
         cls = self.__class__.__name__
-        return f"{cls}(path={self.path!r}, data={self.data!r})"
+        return f"{cls}(path={self.file.path!r}, data={self.data!r})"
 
     def __getitem__(self, key: str) -> str | Any:
         return self.data[key]
 
     # ---
     def save(self):
-        with open(self.path, "w") as stream:
+        with open(self.file.path, "w") as stream:
             stream.write(dumps(self.data, indent=INDENT_SIZE))
 
     def update(self) -> None:
-        self.data = self.load(self.path)
+        self.data = self.load(self.file.path)
